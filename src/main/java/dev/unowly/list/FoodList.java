af3aed054b0e89bc85e883cfc78ae5af0d7e7cc8
unowly <mail@unowly.dev>
2025-05-07 21:10:47 +0200

    add part of foodplus, add bit of datagen (big error), add models and textures

---
+++ src/main/java/dev/unowly/list/FoodList.java
@@ -0,0 +1,93 @@
+package dev.unowly.list;
+
+import net.minecraft.component.type.FoodComponent;
+
+public class FoodList {
+
+//FRUITS AND BUSHES
+public static final FoodComponent AVOCADO = new FoodComponent.Builder()
+        .nutrition(2)
+        .saturationModifier(0.3f)
+        .build();
+
+    public static final FoodComponent BANANA = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent BLUEBERRY = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent CHERRY = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent GRAPE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent KIWI = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent LEMON = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent PEACH = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent PEAR = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent PINEAPPLE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent POMEGRANATE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent STRAWBERRY = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+//VEGETABLES AND CROPS
+    public static final FoodComponent BELL_PEPPER = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent CORN = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent TOMATO = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent LEEK = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+
+    public static final FoodComponent LETTUCE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3f)
+            .build();
+}
