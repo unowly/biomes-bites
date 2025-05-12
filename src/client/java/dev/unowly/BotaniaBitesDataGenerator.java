afe9f3bf71b9980d7cbe92f41bacb4815082f3c8
unowly <mail@unowly.dev>
2025-05-12 12:17:12 +0200

    reverted added feature

--- src/client/java/dev/unowly/BotaniaBitesDataGenerator.java
+++ src/client/java/dev/unowly/BotaniaBitesDataGenerator.java
@@ -1,6 +1,5 @@
 package dev.unowly;
 
-import dev.unowly.provider.BotaniaBitesModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,6 +7,5 @@ public class BotaniaBitesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(BotaniaBitesModelProvider::new);
 	}
 }
--- src/main/java/dev/unowly/BotaniaBitesDataGenerator.java
+++ src/main/java/dev/unowly/BotaniaBitesDataGenerator.java
@@ -1,29 +1,16 @@
 package dev.unowly;
 
-import dev.unowly.data.provider.BotaniaBitesBlockLootTableProvider;
-import dev.unowly.data.provider.BotaniaBitesBlockTagProvider;
-import dev.unowly.data.provider.BotaniaBitesEnglischTranslationProvider;
-import dev.unowly.data.provider.BotaniaBitesItemTagProvider;
-import dev.unowly.worldgen.ConfiguredFeatureRegistry;
-import dev.unowly.worldgen.PlacedFeatureRegistry;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.registry.RegistryBuilder;
-import net.minecraft.registry.RegistryKeys;
 
 public class BotaniaBitesDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-        pack.addProvider(BotaniaBitesBlockLootTableProvider::new);
-        pack.addProvider(BotaniaBitesBlockTagProvider::new);
-        pack.addProvider(BotaniaBitesEnglischTranslationProvider::new);
-        pack.addProvider(BotaniaBitesItemTagProvider::new);
     }
 
     @Override
     public void buildRegistry(RegistryBuilder registryBuilder) {
-        registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ConfiguredFeatureRegistry::bootstrap);
-        registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, PlacedFeatureRegistry::bootstrap);
     }
 }
