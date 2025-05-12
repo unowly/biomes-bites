f83c68f82b2201a96eb757be93fda9d4e0cd3f5f
unowly <mail@unowly.dev>
2025-05-12 12:17:12 +0200

    reverted added feature

--- src/client/java/dev/unowly/BotaniaBitesClientDataGenerator.java
+++ src/client/java/dev/unowly/BotaniaBitesClientDataGenerator.java
@@ -1,6 +1,5 @@
 package dev.unowly;
 
-import dev.unowly.provider.BotaniaBitesModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
