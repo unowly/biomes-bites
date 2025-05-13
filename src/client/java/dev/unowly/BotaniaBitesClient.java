b4542be3e5fae60a526709dcf02c9b9ef2d7290e
unowly <mail@unowly.dev>
2025-05-13 12:08:00 +0200

    rm unused import

--- src/client/java/dev/unowly/BotaniaBitesClient.java
+++ src/client/java/dev/unowly/BotaniaBitesClient.java
@@ -3,7 +3,6 @@ package dev.unowly;
 import dev.unowly.block.BlockRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
 
 public class BotaniaBitesClient implements ClientModInitializer {
