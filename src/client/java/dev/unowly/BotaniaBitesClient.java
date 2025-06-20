27e0812ddb26b073453274adfb16416c7351f858
unowly <mail@unowly.dev>
2025-06-20 23:49:11 +0200

    update to 1.21.6

--- src/client/java/dev/unowly/BotaniaBitesClient.java
+++ src/client/java/dev/unowly/BotaniaBitesClient.java
@@ -2,17 +2,17 @@ package dev.unowly;
 
 import dev.unowly.block.BlockRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class BotaniaBitesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 //BUSHES
-		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), BlockRegistry.BLUEBERRY_BUSH, BlockRegistry.STRAWBERRY_BUSH);
+		BlockRenderLayerMap.putBlocks(BlockRenderLayer.CUTOUT, BlockRegistry.BLUEBERRY_BUSH, BlockRegistry.STRAWBERRY_BUSH);
 
 //CROPS
-		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), BlockRegistry.TOMATO_CROP, BlockRegistry.LEEK_CROP,
+		BlockRenderLayerMap.putBlocks(BlockRenderLayer.CUTOUT, BlockRegistry.TOMATO_CROP, BlockRegistry.LEEK_CROP,
 				BlockRegistry.LETTUCE_CROP, BlockRegistry.CORN_CROP, BlockRegistry.BELL_PEPPER_CROP, BlockRegistry.RICE_CROP);
 	}
 }
