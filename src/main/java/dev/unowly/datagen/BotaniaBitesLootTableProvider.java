919ecd5e72f3fc9c69fee73656115a1b08f30dc4
unowly <mail@robinstahl.net>
2025-07-01 22:40:10 +0200

    add drops for birch, acacia, cherry

--- src/main/java/dev/unowly/datagen/BotaniaBitesLootTableProvider.java
+++ src/main/java/dev/unowly/datagen/BotaniaBitesLootTableProvider.java
@@ -6,9 +6,12 @@ import dev.unowly.item.ItemRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.SurvivesExplosionLootCondition;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
@@ -44,6 +47,31 @@ public class BotaniaBitesLootTableProvider extends FabricBlockLootTableProvider 
 
         addDrop(BlockRegistry.RICE_CROP, createCropLootTable(
                 BlockRegistry.RICE_CROP, ItemRegistry.RICE, ItemRegistry.RICE_SEEDS, RiceCropBlock.AGE));
+
+// LEAVES DROPS
+    // BIRCH
+        addDrop(Blocks.BIRCH_LEAVES, leavesDrops(Blocks.BIRCH_LEAVES, Blocks.BIRCH_SAPLING, 0.05f)
+                .pool(LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(SurvivesExplosionLootCondition.builder())
+                        .with(ItemEntry.builder(ItemRegistry.PEAR)
+                                .conditionally(RandomChanceLootCondition.builder(0.005f)))));
+
+    // CHERRY
+        addDrop(Blocks.CHERRY_LEAVES, leavesDrops(Blocks.CHERRY_LEAVES, Blocks.CHERRY_SAPLING, 0.05f)
+                .pool(LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(SurvivesExplosionLootCondition.builder())
+                        .with(ItemEntry.builder(ItemRegistry.CHERRY)
+                                .conditionally(RandomChanceLootCondition.builder(0.005f)))));
+
+    //SAVANNA
+        addDrop(Blocks.ACACIA_LEAVES, leavesDrops(Blocks.ACACIA_LEAVES, Blocks.ACACIA_SAPLING, 0.05f)
+                .pool(LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(SurvivesExplosionLootCondition.builder())
+                        .with(ItemEntry.builder(ItemRegistry.AVOCADO)
+                                .conditionally(RandomChanceLootCondition.builder(0.005f)))));
 
     }
 
